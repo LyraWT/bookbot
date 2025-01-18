27c61f55248872fbf902778cd9b20b5e992ac69e
LyingRat <lyra_w.t@outlook.com>
2025-01-18 18:08:42 +0100

    Added character count

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     print(text)
     words = word_count(text)
     print(f"Word count: {words}")
+    chars_dict = character_count(text)
+    print(chars_dict)
 
 def get_book_text(book):
     with open(book) as f:
@@ -16,5 +18,18 @@ def word_count(book):
     for word in words:
         counter += 1
     return counter
+
+def character_count(book):
+    dict_book = {}
+    for c in book:
+        uncap = c.lower()
+        if uncap in dict_book:
+            dict_book[uncap] += 1
+        else:
+            dict_book[uncap] = 1
+    return dict_book
+    
+    
+
 
 main()
