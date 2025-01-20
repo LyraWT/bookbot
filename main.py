b884d6e98c8a15ec5b6693f4ee272c530d3539a8
LyingRat <lyra_w.t@outlook.com>
2025-01-20 12:29:54 +0100

    Compiled and formatted the book report

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
     words = word_count(text)
+    print(f"--- Begin report of {book_path} ---")
     print(f"Word count: {words}")
+    print("")
     chars_dict = character_count(text)
-    print(chars_dict)
+    dict_list = book_report(chars_dict)
+    dict_list.sort(reverse=True, key=sort_on)
+    print_out(dict_list)
+    print("--- End report ---")
 
 def get_book_text(book):
     with open(book) as f:
@@ -28,8 +32,24 @@ def character_count(book):
         else:
             dict_book[uncap] = 1
     return dict_book
-    
-    
 
+def sort_on(dict):
+    return dict["count"]
+
+def book_report(dict):
+    report_list = []
+    for entry in dict:
+        if entry.isalpha() == True:
+            count = dict[entry]
+            report_list.append({"character": entry, "count": count})
+            # report_list.append(f"{entry} was found {count} times")
+    return report_list
+
+
+def print_out(dict):
+    for entry in dict:
+        char = entry["character"]
+        count = entry["count"]
+        print(f"The '{char}' character was found {count} times")
 
 main()
